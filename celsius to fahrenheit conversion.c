f075a19dd04736793ab65a6f9105697ccee4ecd4
rishithakeesara <keesararishitha@gmail.com>
2026-09-02 00:03:03 +0530

    Add files via upload

---
+++ celsius to fahrenheit conversion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>;
+int main()
+{
+	float x,fahrenheit;
+	printf("enter x value");
+	scanf("%f",&x);
+	fahrenheit=32+x*9.0/5.0;
+	printf("%f",fahrenheit);
+	return 0;
+}
---
+++ total salrary with bonus.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int salary,bonus,totalsalary;
+	printf("enter salary,bonus values\n");
+	scanf("%d%d",&salary,&bonus);
+	totalsalary=salary+bonus;
+	printf("%d",totalsalary);
+	return 0;
+}
